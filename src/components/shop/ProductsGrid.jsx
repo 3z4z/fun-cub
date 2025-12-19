652163f22158aaefb928883de76f2b82063958f4
Md. Salman Ezaz <md.salmanezaz@gmail.com>
2025-12-20 02:51:13 +0600

    updated product fetch

--- src/components/shop/ProductsGrid.jsx
+++ src/components/shop/ProductsGrid.jsx
@@ -3,7 +3,7 @@ import ProductCardComponent from "./ProductCard";
 export default async function ProductsGridComponent({ products }) {
   return (
     <>
-      <p className="mb-3">Total Products: {prods.length}</p>
+      <p className="mb-3">Total Products: {products.length}</p>
       <div className="grid grid-cols-3 gap-5">
         {products?.map((p, i) => (
           <ProductCardComponent key={i} product={p} />
