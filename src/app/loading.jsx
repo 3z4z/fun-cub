540dece3bc4cc1205531c6e1738d20e572d7bed7
Md. Salman Ezaz <md.salmanezaz@gmail.com>
2025-12-20 03:24:31 +0600

    updated loading iamge

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,7 +4,13 @@ export default function LoadingPage() {
   return (
     <div className="absolute top-0 left-0 w-full h-dvh bg-base-100 flex items-center justify-center">
       <div className="w-50">
-        <Image src={"/loading.gif"} fill className="relative!" alt="" />
+        <Image
+          src={"/loading.gif"}
+          unoptimized
+          fill
+          className="relative!"
+          alt=""
+        />
       </div>
     </div>
   );
