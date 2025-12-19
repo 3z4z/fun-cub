fe0c7c1913adc86be4e7a418f99cfb20d7abd0ef
Md. Salman Ezaz <md.salmanezaz@gmail.com>
2025-12-20 02:34:17 +0600

    updated Navlink

--- src/components/shared/NavLink.jsx
+++ src/components/shared/NavLink.jsx
@@ -2,21 +2,13 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function NavLink({
-  to = "",
-  active = "",
-  children,
-  className = "",
-}) {
+export default function NavLink({ to, active, children, className }) {
   const pathName = usePathname();
-  const isActive =
-    pathName && typeof to === "string" ? pathName.startsWith(to) : false;
-
   return (
     <Link
-      href={to || "/"}
-      className={`transition-all ${className?.trim() || ""} ${
-        isActive ? active?.trim() : ""
+      href={to}
+      className={`transition-all ${className && className.trim()} ${
+        pathName.startsWith(to) && active && active.trim()
       }`}
     >
       {children}
