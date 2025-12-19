03b65646a9248a1980ca251723fd213766d390cd
Md. Salman Ezaz <md.salmanezaz@gmail.com>
2025-12-20 02:17:25 +0600

    updated Navlik

--- src/components/shared/NavLink.jsx
+++ src/components/shared/NavLink.jsx
@@ -4,11 +4,14 @@ import { usePathname } from "next/navigation";
 
 export default function NavLink({ to, active, children, className }) {
   const pathName = usePathname();
+
+  const isActive = pathName ? pathName.startsWith(to) : false;
+
   return (
     <Link
       href={to}
-      className={`transition-all ${className && className.trim()} ${
-        pathName.startsWith(to) && active && active.trim()
+      className={`transition-all ${className?.trim() || ""} ${
+        isActive ? active?.trim() : ""
       }`}
     >
       {children}
